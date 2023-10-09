379650f7ddab378896a703bf49713189df39dec9
Levrk <levrolandkalb@gmail.com>
2023-10-09 17:32:27 -0400

    created skeleton

--- trim.py
+++ trim.py
@@ -1,6 +1,29 @@
 from Bio import SeqIO
 
+
+def main (seq):
+    return printInfo(f3(f2(f1(seq))))
+
+
+def printInfo(seq):
+    print("\n")
+    print("ID: " + seq.id)
+    print("Sequence: " + seq.seq)
+    print("Description: " + seq.description)
+    print("\n")
+    return seq
+
+
 def f1 (seq):
-    print(seq.seq + "\n")
-    print()
-    return
+    #modify seq
+    return seq
+
+
+def f2 (seq):
+    #modify seq
+    return seq
+
+
+def f3 (seq):
+    #modify seq
+    return seq
