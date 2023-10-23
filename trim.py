badc075c53d6cccd22a7af33be0f2e399566e5cb
Levrk <levrolandkalb@gmail.com>
2023-10-23 17:08:28 -0400

    updating main and quality score access

--- trim.py
+++ trim.py
@@ -1,35 +1,45 @@
 from Bio import SeqIO
 
-def main (seq):
-    output = slidingWindow(trim_n(count_n(trailing(leading(seq)))))
-    #printInfo(output)
+def main (seq, count, width, threshold):
+    output = leading(seq, threshold)
+    output = trailing(output,threshold)
+    output = count_n(output,count)
+    output = trim_n(output)
+    output = slidingWindow(output, width, threshold)
+    printInfo(output)
     return output
 
 def printInfo(seq):
     print("\n")
     print("ID: " + seq.id)
     print("Sequence: " + seq.seq)
-    print("Description: " + seq.description)
+    print(seq.letter_annotations['phred_quality'])
     print("\n")
     return seq
 
-
-def slidingWindow (seq):
+def slidingWindow (seq, width, threshold):
     #modify seq
     return seq
 
-def leading (seq):
-    #modify seq
+def leading (seq, threshold):
+    print(threshold)
     return seq
 
-def trailing (seq):
+def trailing (seq, threshold):
     #modify seq
+    print(threshold)
     return seq
 
-def count_n (seq):
+def count_n (seq, count):
     #modify seq
+    print(count)
     return seq
 
 def trim_n (seq):
     #modify seq
     return seq
+
+def quality(char):
+    output = ord(char) - 33
+    return output
+
