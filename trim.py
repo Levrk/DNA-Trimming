4b90798569b1cb67421574e7fdfac414a9b5e3d0
ericlitvack-winkler <104603664+ericlitvack-winkler@users.noreply.github.com>
2023-10-26 18:18:47 -0400

    count_n complete

--- trim.py
+++ trim.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 def main (seq, count, width, threshold):
     output = leading(seq, threshold)
@@ -30,10 +31,20 @@ def trailing (seq, threshold):
     print(threshold)
     return seq
 
+def count_n_helper (seq, count, currCount, index):
+    if currCount > count:
+        seq.seq = Seq("")
+        return seq
+    if index == -1:
+        return seq
+    if seq.seq[index] == "N":
+        return count_n_helper(seq, count, currCount + 1, index - 1)
+    else:
+        return count_n_helper(seq, count, currCount, index - 1)
+
+
 def count_n (seq, count):
-    #modify seq
-    print(count)
-    return seq
+     return count_n_helper (seq, count, 0, len(seq.seq) - 1)   
 
 def trim_n (seq):
     #modify seq
